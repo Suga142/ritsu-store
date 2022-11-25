c652af7908d731d2f40f6754bb29de6a9d49d900
Luis Licon <luisliconna@gmail.com>
2022-11-24 23:05:31 -0300

    navbar agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
     return(
         <div>
-            <h1>Hola Mundo!
-        
-            </h1>
+            <NavBar />
         </div>
     )
 }
